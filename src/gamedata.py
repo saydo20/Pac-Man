fb7ab6a10f2fd0e97525562e14850a322666618d
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-09-20 09:21:59 +0000

    get the locations of the dots (pacgums), and handle if the user enter small number of dots, so i picked up randomly form the list of locations of dots

--- src/gamedata.py
+++ src/gamedata.py
@@ -1,22 +1,18 @@
-import ghost
-import pacman
-import super_pacgums
-import regular_pacgums
+from ghost import Ghost, Color
+from pacman import Pacman
+from super_pacgums import SuperPacgum
+from regular_pacgums import RegularPacgum
 
 from mazegenerator import MazeGenerator
-
 from typing import Dict
 
 
 class GameData:
     def __init__(self, config: Dict) -> None:
-        self.levels = config['levels']
-        self.size_maze = (self.levels[1]['width'],
-                          self.levels[1]['height'])
-
+        # set the maze
+        self.size_maze = (16, 16)
         self.maze = MazeGenerator(self.size_maze, False,
-                                  (0, 0), (-1, -1), config['seed'])
-
+                                  (0, 0), (-1, -1), 42)
         self.grid = self.maze.maze
 
         # initialize the 4 ghosts
@@ -25,63 +21,72 @@ class GameData:
         # initialize pacman and set his start location
         self.__set_pacman(config['lives'])
 
-        # initialize regular pacgums
-        self.__regular_pacgums = regular_pacgums.RegularPacgum()
-
         # initialize super pacgums
-        self.__super_pacgums = super_pacgums.SuperPacgum(self.size_maze)
+        self.__super_pacgums = SuperPacgum(self.size_maze)
         self.super_pacgums.set_super_pacgum_score(
             config['points_per_super_pacgum'])
 
+        # initialize regular pacgums
+        self.__ghosts_position = [
+            self.ghost_red.current_position,
+            self.ghost_yellow.current_position,
+            self.ghost_green.current_position,
+            self.ghost_blue.current_position,
+        ]
+        self.__regular_pacgums = RegularPacgum(
+            config, self.grid, self.pacman.current_position,
+            self.super_pacgums.get_super_pacgums_positions(),
+            self.__ghosts_position)
+
     @property
-    def ghost_red(self) -> ghost.Ghost:
+    def ghost_red(self) -> Ghost:
         return self.__ghost_red
 
     @property
-    def ghost_blue(self) -> ghost.Ghost:
+    def ghost_blue(self) -> Ghost:
         return self.__ghost_blue
 
     @property
-    def ghost_green(self) -> ghost.Ghost:
+    def ghost_green(self) -> Ghost:
         return self.__ghost_green
 
     @property
-    def ghost_yellow(self) -> ghost.Ghost:
+    def ghost_yellow(self) -> Ghost:
         return self.__ghost_yellow
 
     @property
-    def pacman(self) -> pacman.Pacman:
+    def pacman(self) -> Pacman:
         return self.__pacman
 
     @property
-    def regular_pacgums(self) -> regular_pacgums.RegularPacgum:
+    def regular_pacgums(self) -> RegularPacgum:
         return self.__regular_pacgums
 
     @property
-    def super_pacgums(self) -> super_pacgums.SuperPacgum:
+    def super_pacgums(self) -> SuperPacgum:
         return self.__super_pacgums
 
     def __set_the_ghosts(self) -> None:
         # initialize red ghost and his start location
-        self.__ghost_red = ghost.Ghost(self.size_maze, ghost.Color.RED,
-                                       self.grid)
+        self.__ghost_red = Ghost(self.size_maze, Color.RED,
+                                 self.grid)
         self.__ghost_red.set_start_position()
 
         # initialize blue ghost and his start location
-        self.__ghost_blue = ghost.Ghost(self.size_maze, ghost.Color.BLUE,
-                                        self.grid)
+        self.__ghost_blue = Ghost(self.size_maze, Color.BLUE,
+                                  self.grid)
         self.__ghost_blue.set_start_position()
 
         # initialize green ghost and his start location
-        self.__ghost_green = ghost.Ghost(self.size_maze, ghost.Color.GREEN,
-                                         self.grid)
+        self.__ghost_green = Ghost(self.size_maze, Color.GREEN,
+                                   self.grid)
         self.__ghost_green.set_start_position()
 
         # initialize yellow ghost and his start location
-        self.__ghost_yellow = ghost.Ghost(self.size_maze, ghost.Color.YELLOW,
-                                          self.grid)
+        self.__ghost_yellow = Ghost(self.size_maze, Color.YELLOW,
+                                    self.grid)
         self.__ghost_yellow.set_start_position()
 
     def __set_pacman(self, lives: int) -> None:
-        self.__pacman = pacman.Pacman(self.size_maze, self.grid, lives)
+        self.__pacman = Pacman(self.size_maze, self.grid, lives)
         self.__pacman.start_position()
--- src/regular_pacgums.py
+++ src/regular_pacgums.py
@@ -1,2 +1,60 @@
+from typing import List, Dict, Tuple
+import random
+
+
 class RegularPacgum:
-    ...
+    def __init__(self, config: Dict, grid: List[List],
+                 pacman_position: Tuple,
+                 super_pacgums_position: List[Tuple],
+                 ghosts_position: List[Tuple]) -> None:
+
+        # get the position of ghosts, super_pacgums, pacman
+        self.__pacman_position = pacman_position
+        self.__super_pacgums_position = super_pacgums_position
+        self.__ghosts_position = ghosts_position
+
+        # initialize empty list to hold positions of pacgums
+        self.pacgums_positions: List[Tuple] = []
+
+        # get data from config file, and get the available cells for dots
+        self.score_pacgum = config.get('points_per_pacgum', 10)
+        self.nb_available_cells = self.__get_nb_available_cells(grid)
+        self.__pacgums = config.get('pacgum', self.nb_available_cells)
+
+        # get the number of pacgums
+        if self.__pacgums <= self.nb_available_cells:
+            self.nb_pacgums = self.__pacgums
+        else:
+            self.nb_pacgums = self.nb_available_cells
+
+        # get randomly the postions of dots
+        self.pacgums_positions = random.sample(self.pacgums_positions,
+                                               self.nb_pacgums)
+
+    def __is_position_has_superpacgums(self, y: int, x: int) -> bool:
+        for ps in self.__super_pacgums_position:
+            if (x, y) == ps:
+                return True
+        return False
+
+    def __is_position_has_ghost(self, y: int, x: int) -> bool:
+        return (x, y) in self.__ghosts_position
+
+    def __get_nb_available_cells(self, grid: List[List]) -> int:
+        nb_available_cells = 0
+
+        for y in range(len(grid)):
+            for x in range(len(grid[y])):
+                if grid[y][x] == 15:
+                    continue
+                elif (x, y) == self.__pacman_position:
+                    continue
+                elif self.__is_position_has_superpacgums(y, x):
+                    continue
+                elif self.__is_position_has_ghost(y, x):
+                    continue
+                else:
+                    self.pacgums_positions.append((x, y))
+                    nb_available_cells += 1
+
+        return nb_available_cells
